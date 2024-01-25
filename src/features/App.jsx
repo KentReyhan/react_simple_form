a0c3e4e29ba9ea7331bca6493f4132d8d0a44420
kentreyhan <kent196reyhan@gmail.com>
2024-01-25 16:00:36 +0700

    reimplement CSS to Tailwind CSS

--- src/features/App.jsx
+++ src/features/App.jsx
@@ -29,7 +29,7 @@ function App() {
     } else if (values.password.length < 8) {
       errors.password = 'Must be 8 characters or more';
     }
-  
+
     return errors;
   }
 
@@ -48,34 +48,35 @@ function App() {
 
   return (
     <>
-      <div className='background-image-container'></div>
-      <div className="background-container">
-        <div className="form-container">
-          <h2 className="sign-in-text">Do you have account? <a href='https://google.com' id='sign-in-hyperlink'>Sign in</a></h2>
-          <h1 className="form-title">Create Account</h1>
-          <h3 className="form_description">Follow the instructions to make it easier to register and you will be able to explore inside.</h3>
-          <form onSubmit={formik.handleSubmit}>
-            <div className="input-username" >
-              <input type="text" id="userName" name="userName" placeholder="Username" value={formik.values.userName} onChange={formik.handleChange}></input>
-              <img src={userNameIcon} className="icon" />
-              {formik.errors.userName ? <div className="input-error">{formik.errors.userName}</div> : null}
-            </div>
-            <div className="input-email" >
-              <input type="email" name="email" placeholder="Email" value={formik.values.email} onChange={formik.handleChange}></input>
-              <img src={emailIcon} className="icon" />
-              {formik.errors.email ? <div className="input-error">{formik.errors.email}</div> : null}
-            </div>
-            <div className="input-password" >
-              <input type="password" name="password" placeholder="Password" value={formik.values.password} onChange={formik.handleChange}></input>
-              <img src={passwordIcon} className="icon" />
-              {formik.errors.password ? <div className="input-error">{formik.errors.password}</div> : null}
-            </div>
-            <button className='register-button' type="submit">Create Account</button>
-          </form>
-          <div className="third-party-login">
-            <button className="google-button" type="button"><img src={googleIcon} className="icon" />Google</button>
-            <button className="facebook-button" type="button"><img src={facebookIcon} className="icon" />Facebook</button>
+
+      <div className="flex flex-col align-top justify-center bg-gray-100 px-20 pb-24">
+        <h2 className="ml-auto text-end pt-8">Do you have account? <a href='https://google.com' className="text-blue-800">Sign in</a></h2>
+        <h1 className="pt-16">Create Account</h1>
+        <h3 className="py-4">Follow the instructions to make it easier to register and you will be able to explore inside.</h3>
+        <form onSubmit={formik.handleSubmit}>
+          <div className="flex justify-start items-center relative">
+            <input className="box-border w-full my-4 p-4 ps-12 rounded" type="text" id="userName" name="userName" placeholder="Username" value={formik.values.userName} onChange={formik.handleChange}></input>
+            <img src={userNameIcon} className="absolute ml-4 w-5" />
+            {formik.errors.userName ? <div className="input-error">{formik.errors.userName}</div> : null}
           </div>
+          <div className="flex justify-start items-center relative">
+            <input className="box-border w-full my-4 p-4 ps-12 rounded" type="email" name="email" placeholder="Email" value={formik.values.email} onChange={formik.handleChange}></input>
+            <img src={emailIcon} className="absolute ml-4 w-5" />
+            {formik.errors.email ? <div className="input-error">{formik.errors.email}</div> : null}
+          </div>
+          <div className="flex justify-start items-center relative">
+            <input className="box-border w-full my-4 p-4 ps-12 rounded" name="password" placeholder="Password" value={formik.values.password} onChange={formik.handleChange}></input>
+            <img src={passwordIcon} className="absolute ml-4 w-5" />
+            {formik.errors.password ? <div className="input-error">{formik.errors.password}</div> : null}
+          </div>
+          <div className="flex flex-col w-full item-center my-8">
+            <button className="p-4 bg-blue-800 text-white" type="submit">Create Account</button>
+          </div>
+
+        </form>
+        <div className="flex flex-col md:flex-row  box-border justify-center ">
+          <button className="w-full flex text-black text-center items-center bg-white md:mr-4 md:my-0 my-4 p-4 border border-solid border-gray-100" type="button"><img src={googleIcon} className="mr-4" />Google</button>
+          <button className="w-full flex text-black text-center items-center bg-white md:ml-4 md:my-0 my-4 p-4 border border-solid border-gray-100" type="button"><img src={facebookIcon} className="mr-4" />Facebook</button>
         </div>
       </div>
     </>
@@ -83,32 +84,3 @@ function App() {
 }
 
 export default App
-
-/*
-
-<div>
-
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-*/
-
-/*<div className="image-container">
-  <img src={imageForm} className="form-image" alt="Image Form" />
-</div>*/
